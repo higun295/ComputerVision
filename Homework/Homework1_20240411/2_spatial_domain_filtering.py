e670250b54194151e4b59ced62d0474a4d1bc442
HEEKWON <higun295@gmail.com>
2024-04-10 00:54:35 +0900

    doing second homework

--- Homework/Homework1_20240411/2_spatial_domain_filtering.py
+++ Homework/Homework1_20240411/2_spatial_domain_filtering.py
@@ -3,4 +3,14 @@
 # 3. Median Filtering 적용 후 결과 출력
 # 4. Bilateral Filtering 적용 후 결과 출력
 # 5. 각 결과에 대해 노이즈 입히기 전과 절대값 차이를 취해서 결과 출력
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+image = cv2.imread('./data/Lena.png').astype(np.float32) / 255
+
+noised = (image + 0.2 * np.random.rand(*image.shape).astype(np.float32))
+noised = noised.clip(0, 1)
+plt.imshow(noised[:, :, [2, 1, 0]])
+plt.show()
 
